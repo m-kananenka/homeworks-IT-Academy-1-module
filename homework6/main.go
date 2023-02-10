df81b5050d5be5072be66be2bf63e51d83b3193d
spinkymax <sp1nk@mail.ru>
2023-02-10 16:26:48 +0300

    Add homework6 in project

--- homework6/main.go
+++ homework6/main.go
@@ -2,42 +2,71 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"time"
 )
 
 func main() {
 
-	// асинхронный запуск
+	//асинхронный запуск
 	for i := 0; i < 10; i++ {
-		go Fibonacci(i)
+		go PrintNum(i)
 	}
-	time.Sleep(time.Second)
+	time.Sleep(3 * time.Second)
+	fmt.Println()
 
 	//синхронный запуск
-	c := make(chan int, 10)
-	go Fib(cap(c), c)
-	for i := range c {
-		fmt.Println(i)
+	c := make(chan int)
+	go PrintNumSync(0, c)
 
+	for j := range c {
+		fmt.Print(j, " ")
+
+	}
+	fmt.Println()
+
+	//Worker Pools
+	in := make(chan int)
+	out := make(chan int)
+
+	//10 инстансов
+	for i := 0; i < 10; i++ {
+		go PrintNumWp(in, out)
+	}
+	for job := 0; job < 10; job++ {
+		in <- job
+	}
+	close(in)
+
+	//собираем результаты- это гарантирует, что все горутины закончились
+	for k := 0; k < 10; k++ {
+		<-out
 	}
 }
 
-func Fibonacci(n int) int {
-	x, y := 0, 1
-	for i := 0; i < n; i++ {
-		x, y = y, x+y
-	}
-	fmt.Println(x)
-	return x
+func PrintNum(num int) {
+	sum := (num * num) + (num*num)/int(math.Sqrt(3))
+	time.Sleep(2 * time.Second)
+	fmt.Print(sum, " ")
 
 }
 
-// для синхронизации значений пишем в канал с
-func Fib(n int, c chan int) {
-	x, y := 0, 1
-	for i := 0; i < n; i++ {
-		c <- x
-		x, y = y, x+y
+func PrintNumSync(num int, c chan int) {
+	for i := 0; i < 10; i++ {
+		num = i
+		sum := (num * num) + (num*num)/int(math.Sqrt(3))
+		time.Sleep(time.Second * 1)
+		c <- sum
 	}
-	defer close(c)
+	close(c)
+}
+
+func PrintNumWp(in <-chan int, out chan<- int) {
+	for i := range in {
+		sum := (i * i) + (i*i)/int(math.Sqrt(3))
+		time.Sleep(time.Second * 1)
+		fmt.Print(sum, " ")
+		out <- sum
+	}
+
 }
