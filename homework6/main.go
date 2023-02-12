4720a38fd25e0427e7338999148b4bb540b44f72
spinkymax <sp1nk@mail.ru>
2023-02-13 00:55:20 +0300

    final changes in homework6

--- homework6/main.go
+++ homework6/main.go
@@ -3,37 +3,53 @@ package main
 import (
 	"fmt"
 	"sync"
+
 	"time"
 )
 
 func main() {
 
-	//Fibonacci(1000) // 8.4734ms
-	fmt.Println()
-
-	//go Fibonacci(1000)//0s
-	fmt.Println()
-
-	var wg sync.WaitGroup
-
-	for i := 0; i <= 1000; i++ {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			Fibonacci(i) //999.3µs
-		}(i)
-		wg.Wait()
-	}
-}
-
-func Fibonacci(n int) int {
 	defer func(t time.Time) {
 		fmt.Println(time.Since(t))
 	}(time.Now())
+
+	for i := 0; i <= 50000; i++ {
+		Fibonacci(i) //293.1508ms
+	}
+
+	var wg sync.WaitGroup
+	for i := 0; i <= 50000; i++ {
+		wg.Add(1)
+		go func(i int) {
+			SyncFibonacci(i, &wg) //124.0717ms
+		}(i)
+
+	}
+	wg.Wait()
+
+	//семафор
+
+	sem := make(chan struct{}, 10) // где 10 - это количество одновременно работающий горутин
+	for i := 0; i <= 50000; i++ {
+		wg.Add(1)
+		sem <- struct{}{}
+		go func(i int) {
+			SyncFibonacci(i, &wg) //74.5442ms
+			<-sem
+		}(i)
+	}
+	wg.Wait()
+}
+
+func Fibonacci(n int) {
 	x, y := 0, 1
 	for i := 0; i < n; i++ {
 		x, y = y, x+y
-		fmt.Print(x, " ")
 	}
-	return x
+
+}
+
+func SyncFibonacci(n int, wg *sync.WaitGroup) {
+	Fibonacci(n)
+	wg.Done()
 }
